3098ac479bac17cd030af32fb8e5cf1ee0dcbb2e
Neonila Shlikhtenko <shlikhtenkoneonila@gmail.com>
2014-04-23 14:22:02 +0300

    make nav-multiple

--- js/main.js
+++ js/main.js
@@ -7,4 +7,20 @@ $(document).ready(function(){
 		$(".slides-video-big li").removeClass("is-active");
 		$(".slides-video-big").find("." + activeSlide + "-big").addClass("is-active");
 	});
+
+	//Multiple navigation
+
+	$(".has-nav-inner").hover(
+		function() {
+			$(this).addClass("block-shadowed");
+			$(this).children("a").addClass("is-open");
+			$(this).find(".nav-inner").show();
+		},
+
+		function() {
+			$(this).removeClass("block-shadowed");
+			$(this).find(".nav-inner").hide();
+			$(this).children("a").removeClass("is-open");
+		}
+	);
 });
